28ebd6a33a23cd5da5a7d3e97eec4f7395e2fadb
jamesheck2019 <48408300+jamesheck2019@users.noreply.github.com>
2020-03-25 17:40:12 +0200

    Delete ReportStatus.vb

--- src/keep2shareSDK/keep2shareSDK/Cls/ReportStatus.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class ReportStatus
-    Property ProgressPercentage As String
-    Property BytesTransferred As String
-    Property TotalBytes As String
-    Property TextStatus As String
-    Property Finished As Boolean
-End Class
