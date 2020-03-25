1b1fd818e70158ebaa884ffde33b66fadeb64b81
jamesheck2019 <48408300+jamesheck2019@users.noreply.github.com>
2020-03-25 17:40:20 +0200

    Delete ProxyConfig.vb

--- src/keep2shareSDK/keep2shareSDK/Cls/ProxyConfig.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class ProxyConfig
-    Property SetProxy As Boolean
-    Property ProxyIP As String
-    Property ProxyPort As String
-    Property ProxyUsername As String
-    Property ProxyPassword As String
-End Class
